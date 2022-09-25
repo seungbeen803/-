b0aef148d4dae2de201c2433dc01f53160f3e331
seungbeen <seungbin803@gmail.com>
2022-09-26 00:27:35 +0900

    9월 26일  수행평가 준비 완료

--- 코딩테스트 수행준비/수행평가 대비.c
+++ 코딩테스트 수행준비/수행평가 대비.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
 int main(void) {
+
 	char str[30] = "I want go home";
 
 	// 문자열 길이
 	int length = strlen(str);
-	printf("문자열의 길이 : %d\n", length);
+	printf("문자열의 길이 : %d\n");
 
 	// 문자열 복사
 	char copy_str[30];
 	strcpy(copy_str, str);
 	printf("%s \n", copy_str);
+
 	// 문자열 덧붙이기
+	strcat(copy_str, " right now!");
+	printf("%s \n", copy_str);
+
 
 	// 문자열 비교하기
 	if (strcmp("BMW", "benz") == 0)
